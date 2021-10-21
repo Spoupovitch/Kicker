96e7118605409b24f9535f90427d53b16bf0d759
Spoupovitch <Spoupovitch@gmail.com>
2021-10-21 00:47:40 -0700

    modify index routing

--- app.js
+++ app.js
@@ -18,18 +18,11 @@ app.use(express.static(path.join(__dirname, 'assets')));
 // Parser for URL-encoded bodies from forms
 app.use(express.urlencoded({extended: false}));
 // Parser for JSON bodies from API client
-app.use(express.json());
 app.use(bodyParser.json());
 
 // Template engine
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
-
-app.post("/addTask", (req, res) => {
-    res.send("POST to table");
-    console.log(req);
-    console.log(res);
-});
 
 app.listen(process.env.APP_PORT, () => {
     console.log(process.env.INFO);
